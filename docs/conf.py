0bec0c2b4a9c1427e3c7a2f6e5c02003e5547082
adrinjalali <adrin.jalali@gmail.com>
2022-05-05 14:05:31 +0200

    DOC enable sphinx-gallery

--- docs/conf.py
+++ docs/conf.py
@@ -35,10 +35,15 @@ release = ".".join(parsed_version.base_version.split(".")[:2])
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "numpydoc"]
+extensions = ["sphinx.ext.autodoc", "numpydoc", "sphinx_gallery.gen_gallery"]
+
 autodoc_default_options = {"members": True, "inherited-members": True}
 autodoc_typehints = "description"
 
+sphinx_gallery_conf = {
+    "examples_dirs": "../examples",  # path to your example scripts
+    "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
+}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
