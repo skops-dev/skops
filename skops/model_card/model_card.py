9d475328a6617d1a2bc38b8b1236a84362e2f4a7
Merve Noyan <merveenoyan@gmail.com>
2022-05-12 14:09:03 +0200

    Update skops/model_card/model_card.py
    Co-authored-by: Adrin Jalali <adrin.jalali@gmail.com>

--- skops/model_card/model_card.py
+++ skops/model_card/model_card.py
@@ -3,7 +3,7 @@ from typing import Union
 
 
 def create_model_card(*, path: Union[str, Path], model):
-    """Write a model card for the model and save it.
+    """Generate a model card for the model and save it.
 
     This function takes the path to the repo and a model, generate
     a model card and saves it in the repository.
