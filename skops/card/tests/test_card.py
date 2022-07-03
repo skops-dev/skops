266e21299c1b9edd5e15e6ad2401819812dff94d
merveenoyan <merveenoyan@gmail.com>
2022-07-03 17:03:35 +0200

    added tests

--- skops/card/tests/test_card.py
+++ skops/card/tests/test_card.py
@@ -1,7 +1,8 @@
 import os
+import tempfile
 from pathlib import Path
 
-HF_HUB_TOKEN = os.environ.get("HF_HUB_TOKEN", None)
+from skops import card
 
 
 def _get_cwd():
@@ -12,21 +13,64 @@ def _get_cwd():
     return Path(os.getenv("PYTEST_CURRENT_TEST").split("::")[0]).parent
 
 
+def fit_model():
+    import numpy as np
+    from sklearn.linear_model import LinearRegression
+
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+    reg = LinearRegression().fit(X, y)
+    return reg
+
+
+def write_card():
+    model = fit_model()
+    model_card = card.create_model_card(model, model_description="sklearn FTW")
+    return model_card
+
+
 def test_write_model_card():
-    # Check if the model card is written
-    pass
+    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
+        model = fit_model()
+        model_card = card.create_model_card(model, model_description="sklearn FTW")
+        model_card.save(f"{dir_path}/README.md")
+        with open(f"{dir_path}/README.md", "r") as f:
+            model_card = f.read()
+        assert "sklearn FTW" in model_card
 
 
 def test_hyperparameter_table():
-    # Check if table is in model card
-    pass
+    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
+        model_card = write_card()
+        model_card.save(f"{dir_path}/README.md")
+        with open(f"{dir_path}/README.md", "r") as f:
+            model_card = f.read()
+        assert "fit_intercept" in model_card
 
 
 def test_hyperparameter_table_search():
-    # Check if hyperparameters are extracted for non-trivial hyperparameters are there
+    """
+    from sklearn.metrics import r2_score, make_scorer
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.tree import DecisionTreeRegressor
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+    gs = GridSearchCV(
+    DecisionTreeClassifier(random_state=42),
+    param_grid={"max_depth": [1, 2]},
+    scoring={"R_square": make_scorer(r2_score)},
+    refit="R_square",
+    n_jobs=1,
+    )
+    gs.fit(X, y)
+    """
     pass
 
 
 def test_plot_model():
-    # Check if model plot is in model card
-    pass
+    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
+        model_card = write_card()
+        model_card.save(f"{dir_path}/README.md")
+        with open(f"{dir_path}/README.md", "r") as f:
+            model_card = f.read()
+        assert "<style>" in model_card
