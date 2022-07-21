af6642cdbc60346e4ce015b96ecbd94e8a5105aa
adrinjalali <adrin.jalali@gmail.com>
2022-07-21 10:45:10 +0200

    MNT add clean script to remove all repos on skops user

--- scripts/clean_skops.py
+++ scripts/clean_skops.py
@@ -1,8 +1,18 @@
+"""This script removes all repos under skops user.
+
+The user is used for the CI and if there are leftover repos, they can be
+removed.
+"""
+
 from huggingface_hub import HfApi
 
+# This is the token for the skops user.
 token = "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"
 client = HfApi()
 user = client.whoami(token=token)["name"]
+answer = input(f"Are you sure you want to delete all repos under {user}? (y/n)")
+if answer != "y":
+    exit(1)
 models = client.list_models(author=user)
 for model_info in models:
     print(f"deleting {model_info.modelId}")
