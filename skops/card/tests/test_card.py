01c507bddf0cdcaf37ef233f67ac13f657e8031d
adrinjalali <adrin.jalali@gmail.com>
2022-07-25 13:59:09 +0200

    fix model card tests

--- skops/card/tests/test_card.py
+++ skops/card/tests/test_card.py
@@ -1,26 +1,48 @@
-import os
+import pickle
 import tempfile
+from pathlib import Path
 
 import numpy as np
+import pytest
+import sklearn
 from modelcards import CardData
 from sklearn.linear_model import LinearRegression
 
+from skops import hub_utils
 from skops.card import create_model_card
 
 
-def fit_model():
+@pytest.fixture
+def temp_path():
+    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
+        yield Path(dir_path)
+
+
+@pytest.fixture
+def model_folder(temp_path):
     X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
     y = np.dot(X, np.array([1, 2])) + 3
     reg = LinearRegression().fit(X, y)
-    return reg
+    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
+        model_path = Path(dir_path) / "model.pkl"
+        with open(model_path, "wb") as f:
+            pickle.dump(reg, f)
+        hub_utils.init(
+            model=model_path,
+            requirements=[f"scikit-learn{sklearn.__version__}"],
+            task="tabular-classification",
+            data=X,
+            dst=temp_path,
+        )
+    yield Path(temp_path)
 
 
-def generate_card():
-    model = fit_model()
+@pytest.fixture
+def sample_card(model_folder):
     card_data = CardData(library_name="sklearn")
 
     model_card = create_model_card(
-        model,
+        model_folder,
         card_data,
         template_path="skops/card/default_template.md",
         model_description="sklearn FTW",
@@ -28,32 +50,31 @@ def generate_card():
     return model_card
 
 
-def test_write_model_card():
-    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
-        model = fit_model()
-        card_data = CardData(library_name="sklearn")
-        model_card = create_model_card(
-            model, card_data=card_data, model_description="sklearn FTW"
-        )
-        model_card.save(os.path.join(f"{dir_path}", "README.md"))
-        with open(os.path.join(f"{dir_path}", "README.md"), "r") as f:
-            model_card = f.read()
-        assert "sklearn FTW" in model_card
+def test_write_model_card(model_folder):
+    card_data = CardData(library_name="sklearn")
+    model_card = create_model_card(
+        model_folder,
+        card_data=card_data,
+        model_description="sklearn FTW",
+    )
+    card_path = model_folder / "README.md"
+    model_card.save(card_path)
+    with open(card_path, "r") as f:
+        model_card = f.read()
+    assert "sklearn FTW" in model_card
 
 
-def test_hyperparameter_table():
-    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
-        model_card = generate_card()
-        model_card.save(os.path.join(f"{dir_path}", "README.md"))
-        with open(os.path.join(f"{dir_path}", "README.md"), "r") as f:
-            model_card = f.read()
-        assert "fit_intercept" in model_card
+def test_hyperparameter_table(sample_card, temp_path):
+    card_path = temp_path / "README.md"
+    sample_card.save(card_path)
+    with open(card_path, "r") as f:
+        model_card = f.read()
+    assert "fit_intercept" in model_card
 
 
-def test_plot_model():
-    with tempfile.TemporaryDirectory(prefix="skops-test") as dir_path:
-        model_card = generate_card()
-        model_card.save(os.path.join(f"{dir_path}", "README.md"))
-        with open(os.path.join(f"{dir_path}", "README.md"), "r") as f:
-            model_card = f.read()
-        assert "<style>" in model_card
+def test_plot_model(sample_card, temp_path):
+    card_path = temp_path / "README.md"
+    sample_card.save(card_path)
+    with open(card_path, "r") as f:
+        model_card = f.read()
+    assert "<style>" in model_card
