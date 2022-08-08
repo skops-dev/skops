c1c95c5f15ca5a101d14e21e876262aae03e9afb
merveenoyan <merveenoyan@gmail.com>
2022-08-08 11:59:21 +0200

    nits

--- skops/card/_model_card.py
+++ skops/card/_model_card.py
@@ -86,7 +86,7 @@ class Card:
 
     def __init__(self, model: Any, model_diagram: bool = True) -> None:
         self.model = model
-        self._hyperparameter_table = self._write_hyperparameters()
+        self._hyperparameter_table = self._extract_estimator_config()
         # the spaces in the pipeline breaks markdown, so we replace them
         if model_diagram is True:
             self._model_plot: str | None = re.sub(
@@ -206,7 +206,7 @@ class Card:
 
         card.save(path)
 
-    def _write_hyperparameters(self) -> str:
+    def _extract_estimator_config(self) -> str:
         """Extracts estimator hyperparameters and renders them into a vertical table.
         Returns
         -------
@@ -215,7 +215,7 @@ class Card:
         """
         hyperparameter_dict = self.model.get_params(deep=True)
         return tabulate(
-            [[k, v] for k, v in hyperparameter_dict.items()],
+            list(hyperparameter_dict.items()),
             headers=["Hyperparameter", "Value"],
             tablefmt="github",
         )
