438e89aedb193682af1d65fa637555b791fc61a1
merveenoyan <merveenoyan@gmail.com>
2022-08-09 12:21:45 +0200

    fixed my test

--- skops/card/tests/test_card.py
+++ skops/card/tests/test_card.py
@@ -115,7 +115,7 @@ def test_metadata_keys(destination_path, model_card):
 
 
 def test_evaluate_metric(destination_path, model_card):
-    model_card.evaluate({"acc": 0.1, "f1": 0.02})
+    model_card.add_metrics({"acc": 0.1, "f1": 0.02})
     model_card.save(Path(destination_path) / "README.md")
     with open(Path(destination_path) / "README.md", "r") as f:
         assert "acc" and "f1" and "0.1" in f.read()
