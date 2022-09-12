e5e3b96fa36cc1d1d6cb3e5fae0ad88d7edd8bed
merveenoyan <merveenoyan@gmail.com>
2022-09-12 15:26:24 +0200

    removed gradio from plot_hf_hub and have separate example

--- docs/index.rst
+++ docs/index.rst
@@ -23,6 +23,8 @@ The following examples are good starting points:
   :ref:`sphx_glr_auto_examples_plot_model_card.py`
 - A text classification example, and its integration with the hub:
   :ref:`sphx_glr_auto_examples_plot_text_classification.py`
+- How to build user interfaces to your models using Gradio integration
+  of skops :ref:`sphx_glr_auto_examples_plot_gradio.py`
 
 In order to better understand the role of each file and their content when
 uploaded to Hugging Face Hub, refer to this :ref:`user guide <hf_hub>`. You can
--- examples/plot_hf_hub.py
+++ examples/plot_hf_hub.py
@@ -163,14 +163,6 @@ print(json.dumps(hub_utils.get_config(path=repo_copy), indent=2))
 
 hub_utils.update_env(path=local_repo, requirements=["scikit-learn"])
 
-# %%
-# ``gradio`` is a python library that lets you create interfaces on your model.
-# It is integrated with skops, so you can load an interface with only one line
-# of code like below. You can further customize your UI, add description, title,
-# and more. You can later share your application on Hugging Face Spaces.
-# For more information, please refer to documentation of ``gradio``.
-
-gr.Interface.load(f"huggingface/{repo_id}").launch()
 
 
 # %%
