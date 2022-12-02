9744998d0d80d358b599a14ecc943e47f0dae326
Benjamin Bossan <benjamin.bossan@gmail.com>
2022-12-02 14:30:16 +0100

    Add a bunch of more markup support
    Now supports:
    
    - Space
    - Strong
    - Emph
    - Strikeout
    - Subscript
    - Superscript
    - Plain
    - Str
    - RawInline
    - RawBlock
    - SoftBreak
    - LineBreak
    - Para
    - Header
    - Image
    - CodeBlock
    - Code
    - Table
    - Div
    - Link
    - BulletList
    - Quoted

--- skops/card/_markup.py
+++ skops/card/_markup.py
@@ -17,7 +17,7 @@ class Markdown:
     This class has a ``mapping`` attribute, which is just a dict. The keys are
     Pandoc types and the values are functions that transform the corresponding
     value into a string with markdown syntax. Those functions are all prefixed
-    with ``md_``, e.g. ``md_Image`` for transforming a pandoc ``Image`` into a
+    with ``md_``, e.g. ``md_image`` for transforming a pandoc ``Image`` into a
     markdown figure.
 
     From the caller side, only the ``__call__`` method should be used, the rest
@@ -30,17 +30,26 @@ class Markdown:
         self.mapping = {
             "Space": self.md_space,
             "Strong": self.md_strong,
+            "Emph": self.md_emph,
+            "Strikeout": self.md_strikeout,
+            "Subscript": self.md_subscript,
+            "Superscript": self.md_superscript,
             "Plain": self.md_plain,
             "Str": self.md_str,
             "RawInline": self.md_rawline,
             "RawBlock": self.md_raw_block,
             "SoftBreak": self.md_softbreak,
+            "LineBreak": self.md_linebreak,
             "Para": self.md_para,
             "Header": self.md_header,
             "Image": self.md_image,
             "CodeBlock": self.md_code_block,
+            "Code": self.md_code,
             "Table": self.md_table,
             "Div": self.md_parse_div,
+            "Link": self.md_link,
+            "BulletList": self.md_bullet_list,
+            "Quoted": self.md_quoted,
         }
 
     @staticmethod
@@ -51,6 +60,30 @@ class Markdown:
         parts = ["**"]
         parts += [self.__call__(subitem) for subitem in value]
         parts.append("**")
+        return "".join(parts)
+
+    def md_emph(self, value) -> str:
+        parts = ["_"]
+        parts += [self.__call__(subitem) for subitem in value]
+        parts.append("_")
+        return "".join(parts)
+
+    def md_strikeout(self, value) -> str:
+        parts = ["~~"]
+        parts += [self.__call__(subitem) for subitem in value]
+        parts.append("~~")
+        return "".join(parts)
+
+    def md_subscript(self, value) -> str:
+        parts = ["<sub>"]
+        parts += [self.__call__(subitem) for subitem in value]
+        parts.append("</sub>")
+        return "".join(parts)
+
+    def md_superscript(self, value) -> str:
+        parts = ["<sup>"]
+        parts += [self.__call__(subitem) for subitem in value]
+        parts.append("</sup>")
         return "".join(parts)
 
     def md_plain(self, value) -> str:
@@ -74,6 +107,10 @@ class Markdown:
 
     @staticmethod
     def md_softbreak(value) -> str:
+        return "\n"
+
+    @staticmethod
+    def md_linebreak(value) -> str:
         return "\n"
 
     def _make_content(self, content):
@@ -105,13 +142,18 @@ class Markdown:
     @staticmethod
     def md_code_block(item: tuple[tuple[int, list[str], list[str]], str]) -> str:
         # a codeblock consists of: (id, classes, namevals) contents
-        (_, _, namevals), content = item
+        (_, classes, _), content = item
         block_start = "```"
-        if namevals:  # TODO: check if this makes "```python" etc.
-            block_start += namevals[0]
+        if classes:
+            block_start += ", ".join(classes)
         block_end = "```"
         content = "\n".join((block_start, content, block_end))
         return content
+
+    @staticmethod
+    def md_code(item: tuple[Any, str]) -> str:
+        _, txt = item
+        return f"`{txt}`"
 
     def md_table(self, item) -> str:
         _, alignments, _, header, rows = item
@@ -161,6 +203,36 @@ class Markdown:
             middle.append(self.__call__(content))
         end = "</div>"
         return "".join([start] + middle + [end])
+
+    def md_link(self, item) -> str:
+        _, txt, (src, _) = item
+        txt_formatted = self._make_content(txt)
+        return f"[{txt_formatted}]({src})"
+
+    def md_bullet_list(self, item) -> str:
+        parts = []
+        for subitem in item:
+            assert len(subitem) == 1
+            content = "".join(self.__call__(i) for i in subitem)
+            # indent the lines in lists if they contain line breaks
+            content = content.replace("\n", "\n  ")
+            parts.append(f"- {content}")
+        return "\n".join(parts)
+
+    def md_quoted(self, item: tuple[dict[str, str], list[PandocItem]]) -> str:
+        quote_type, content = item
+        type_ = quote_type["t"]
+        try:
+            sym = {"DoubleQuote": '"', "SingleQuote": "'"}[type_]
+        except KeyError as exc:
+            msg = (
+                f"The parsed document contains '{type_}', which is not "
+                "supported yet, please open an issue on GitHub"
+            )
+            raise ValueError(msg) from exc
+
+        text = "".join(self.__call__(i) for i in content)
+        return f"{sym}{text}{sym}"
 
     def __call__(self, item: str | PandocItem) -> str:
         if isinstance(item, str):
