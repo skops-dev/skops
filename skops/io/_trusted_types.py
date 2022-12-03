0e600b81c78e3ce9ec547695c28789c29536eb4a
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2022-12-03 19:06:11 +0100

    added simple sanity check

--- skops/io/_trusted_types.py
+++ skops/io/_trusted_types.py
@@ -7,5 +7,7 @@ PRIMITIVES_TYPES = [int, float, str, bool]
 PRIMITIVE_TYPE_NAMES = ["builtins." + t.__name__ for t in PRIMITIVES_TYPES]
 
 SKLEARN_ESTIMATOR_TYPE_NAMES = [
-    get_type_name(estimator_class) for _, estimator_class in all_estimators()
+    get_type_name(estimator_class)
+    for _, estimator_class in all_estimators()
+    if get_type_name(estimator_class).startswith("sklearn.")
 ]
