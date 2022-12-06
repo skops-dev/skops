5e5c1a35324b3fb393751cad8331ef4ad45dabcf
Benjamin Bossan <benjamin.bossan@gmail.com>
2022-12-06 11:10:40 +0100

    Rename Markdown class's methods, make private

--- skops/card/_markup.py
+++ skops/card/_markup.py
@@ -17,8 +17,8 @@ class Markdown:
     This class has a ``mapping`` attribute, which is just a dict. The keys are
     Pandoc types and the values are functions that transform the corresponding
     value into a string with markdown syntax. Those functions are all prefixed
-    with ``md_``, e.g. ``md_image`` for transforming a pandoc ``Image`` into a
-    markdown figure.
+    with ``_``, e.g. ``_image`` for transforming a pandoc ``Image`` into a
+    markdown figure, or ``_raw_block``, to transform a pandoc ``RawBlock``.
 
     From the caller side, only the ``__call__`` method should be used, the rest
     should be considered internals.
@@ -28,91 +28,91 @@ class Markdown:
     def __init__(self):
         # markdown syntax dispatch table
         self.mapping = {
-            "Space": self.md_space,
-            "Strong": self.md_strong,
-            "Emph": self.md_emph,
-            "Strikeout": self.md_strikeout,
-            "Subscript": self.md_subscript,
-            "Superscript": self.md_superscript,
-            "Plain": self.md_plain,
-            "Str": self.md_str,
-            "RawInline": self.md_rawline,
-            "RawBlock": self.md_raw_block,
-            "SoftBreak": self.md_softbreak,
-            "LineBreak": self.md_linebreak,
-            "Para": self.md_para,
-            "Header": self.md_header,
-            "Image": self.md_image,
-            "CodeBlock": self.md_code_block,
-            "Code": self.md_code,
-            "Table": self.md_table,
-            "Div": self.md_parse_div,
-            "Link": self.md_link,
-            "BulletList": self.md_bullet_list,
-            "Quoted": self.md_quoted,
-            "BlockQuote": self.md_block_quote,
+            "Space": self._space,
+            "Strong": self._strong,
+            "Emph": self._emph,
+            "Strikeout": self._strikeout,
+            "Subscript": self._subscript,
+            "Superscript": self._superscript,
+            "Plain": self._plain,
+            "Str": self._str,
+            "RawInline": self._raw_inline,
+            "RawBlock": self._raw_block,
+            "SoftBreak": self._soft_break,
+            "LineBreak": self._line_break,
+            "Para": self._para,
+            "Header": self._header,
+            "Image": self._image,
+            "CodeBlock": self._code_block,
+            "Code": self._code,
+            "Table": self._table,
+            "Div": self._parse_div,
+            "Link": self._link,
+            "BulletList": self._bullet_list,
+            "Quoted": self._quoted,
+            "BlockQuote": self._block_quote,
         }
 
     @staticmethod
-    def md_space(value) -> str:
+    def _space(value) -> str:
         return " "
 
-    def md_strong(self, value) -> str:
+    def _strong(self, value) -> str:
         parts = ["**"]
         parts += [self.__call__(subitem) for subitem in value]
         parts.append("**")
         return "".join(parts)
 
-    def md_emph(self, value) -> str:
+    def _emph(self, value) -> str:
         parts = ["_"]
         parts += [self.__call__(subitem) for subitem in value]
         parts.append("_")
         return "".join(parts)
 
-    def md_strikeout(self, value) -> str:
+    def _strikeout(self, value) -> str:
         parts = ["~~"]
         parts += [self.__call__(subitem) for subitem in value]
         parts.append("~~")
         return "".join(parts)
 
-    def md_subscript(self, value) -> str:
+    def _subscript(self, value) -> str:
         parts = ["<sub>"]
         parts += [self.__call__(subitem) for subitem in value]
         parts.append("</sub>")
         return "".join(parts)
 
-    def md_superscript(self, value) -> str:
+    def _superscript(self, value) -> str:
         parts = ["<sup>"]
         parts += [self.__call__(subitem) for subitem in value]
         parts.append("</sup>")
         return "".join(parts)
 
-    def md_plain(self, value) -> str:
+    def _plain(self, value) -> str:
         parts = [self.__call__(subitem) for subitem in value]
         return "".join(parts)
 
     @staticmethod
-    def md_str(value) -> str:
+    def _str(value) -> str:
         # escape \
         return value.replace("\\", "\\\\")
 
     @staticmethod
-    def md_rawline(value) -> str:
+    def _raw_inline(value) -> str:
         _, line = value
         return line
 
-    def md_raw_block(self, item) -> str:
+    def _raw_block(self, item) -> str:
         # throw away the first item, which is just something like 'html'
         # might have to revisit this if output != markdown
         _, line = item
         return line
 
     @staticmethod
-    def md_softbreak(value) -> str:
+    def _soft_break(value) -> str:
         return "\n"
 
     @staticmethod
-    def md_linebreak(value) -> str:
+    def _line_break(value) -> str:
         return "\n"
 
     def _make_content(self, content):
@@ -122,16 +122,16 @@ class Markdown:
             parts.append(part)
         return "".join(parts)
 
-    def md_para(self, value: list[dict[str, str]]) -> str:
+    def _para(self, value: list[dict[str, str]]) -> str:
         content = self._make_content(value)
         return content
 
-    def md_header(self, value: tuple[int, Any, list[dict[str, str]]]) -> str:
+    def _header(self, value: tuple[int, Any, list[dict[str, str]]]) -> str:
         level, _, content_parts = value
         section_name = self._make_content(content_parts)
         return section_name
 
-    def md_image(self, value) -> str:
+    def _image(self, value) -> str:
         (ident, _, keyvals), caption, (dest, typef) = value
         # it seems like ident and keyvals are not relevant for markdown
         assert caption
@@ -142,7 +142,7 @@ class Markdown:
         return content
 
     @staticmethod
-    def md_code_block(item: tuple[tuple[int, list[str], list[str]], str]) -> str:
+    def _code_block(item: tuple[tuple[int, list[str], list[str]], str]) -> str:
         # a codeblock consists of: (id, classes, namevals) contents
         (_, classes, _), content = item
         block_start = "```"
@@ -153,11 +153,11 @@ class Markdown:
         return content
 
     @staticmethod
-    def md_code(item: tuple[Any, str]) -> str:
+    def _code(item: tuple[Any, str]) -> str:
         _, txt = item
         return f"`{txt}`"
 
-    def md_table(self, item) -> str:
+    def _table(self, item) -> str:
         _, alignments, _, header, rows = item
         fn = self.__call__
         columns = ["".join(fn(part) for part in col) for col in header]
@@ -178,7 +178,7 @@ class Markdown:
         res = TableSection(table).format()
         return res
 
-    def md_parse_div(self, item) -> str:
+    def _parse_div(self, item) -> str:
         # note that in markdown, we basically just use the raw html
         (ident, classes, kvs), contents = item
 
@@ -206,12 +206,12 @@ class Markdown:
         end = "</div>"
         return "".join([start] + middle + [end])
 
-    def md_link(self, item) -> str:
+    def _link(self, item) -> str:
         _, txt, (src, _) = item
         txt_formatted = self._make_content(txt)
         return f"[{txt_formatted}]({src})"
 
-    def md_bullet_list(self, item) -> str:
+    def _bullet_list(self, item) -> str:
         parts = []
         for subitem in item:
             assert len(subitem) == 1
@@ -221,7 +221,7 @@ class Markdown:
             parts.append(f"- {content}")
         return "\n".join(parts)
 
-    def md_quoted(self, item: tuple[dict[str, str], list[PandocItem]]) -> str:
+    def _quoted(self, item: tuple[dict[str, str], list[PandocItem]]) -> str:
         quote_type, content = item
         type_ = quote_type["t"]
         try:
@@ -236,7 +236,7 @@ class Markdown:
         text = "".join(self.__call__(i) for i in content)
         return f"{sym}{text}{sym}"
 
-    def md_block_quote(self, item: list[PandocItem]) -> str:
+    def _block_quote(self, item: list[PandocItem]) -> str:
         parts = []
         for subitem in item:
             content = self.__call__(subitem)
