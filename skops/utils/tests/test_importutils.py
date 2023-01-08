89801e346d2dd038f1d570830384d344f8c7d4ed
BenjaminBossan <b.bossan@gmail.com>
2023-01-08 12:06:32 +0100

    Test Merve's importutils on GH CI

---
+++ skops/utils/tests/test_importutils.py
@@ -0,0 +1,30 @@
+import builtins
+
+import pytest
+
+from skops.utils.importutils import import_or_raise
+
+
+@pytest.fixture
+def hide_available_matplotlib(monkeypatch):
+    import_orig = builtins.__import__
+
+    def mocked_import(name, *args, **kwargs):
+        if name == "matplotlib":
+            raise ImportError()
+        return import_orig(name, *args, **kwargs)
+
+    monkeypatch.setattr(builtins, "__import__", mocked_import)
+
+
+@pytest.mark.usefixtures("hide_available_matplotlib")
+def test_import_or_raise():
+    with pytest.raises(
+        ModuleNotFoundError,
+        match=(
+            "Permutation importance requires matplotlib to be installed. In order"
+            " to use permutation importance, you need to install the package in"
+            " your current python environment."
+        ),
+    ):
+        import_or_raise("matplotlib", "permutation importance")
