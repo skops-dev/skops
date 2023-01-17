ff9c7bf59cdedb086e8e8a6a685cb794e4f34e03
merveenoyan <merveenoyan@gmail.com>
2023-01-17 16:14:09 +0100

    removed redundant fixture

--- skops/utils/tests/test_importutils.py
+++ skops/utils/tests/test_importutils.py
@@ -1,27 +1,6 @@
-import builtins
-
 import pytest
 
 from skops.utils.importutils import import_or_raise
-
-
-@pytest.fixture
-def hide_available_matplotlib(monkeypatch):
-    import_orig = builtins.__import__
-
-    # ugly way of removing matplotlib from cached imports
-    import sys
-
-    for key in list(sys.modules.keys()):
-        if key.startswith("matplotlib"):
-            del sys.modules[key]
-
-    def mocked_import(name, *args, **kwargs):
-        if name == "matplotlib":
-            raise ImportError()
-        return import_orig(name, *args, **kwargs)
-
-    monkeypatch.setattr(builtins, "__import__", mocked_import)
 
 
 @pytest.mark.usefixtures("matplotlib_not_installed")
