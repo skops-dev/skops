dc460df532e6733b662ab07b836b7d6a4a0e3730
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-01-20 12:08:15 +0100

    Test huggingface_hub v0.12.0rc0, don't merge!

--- skops/_min_dependencies.py
+++ skops/_min_dependencies.py
@@ -11,7 +11,7 @@ PYTEST_MIN_VERSION = "5.0.1"
 #     "tomli": ("1.1.0", "install", "python_full_version < '3.11.0a7'"),
 dependent_packages = {
     "scikit-learn": ("0.24", "install", None),
-    "huggingface_hub": ("0.10.1", "install", None),
+    "huggingface_hub": ("0.12.0rc0", "install", None),
     "tabulate": ("0.8.8", "install", None),
     "pytest": (PYTEST_MIN_VERSION, "tests", None),
     "pytest-cov": ("2.9.0", "tests", None),
