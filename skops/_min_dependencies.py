240876f7a8299fbd15490c3612581a4f11bb1ae4
Thomas Lazarus <lazarustda@gmail.com>
2023-02-04 12:14:12 -0600

    Adds fairlearn to _min_dependencies

--- skops/_min_dependencies.py
+++ skops/_min_dependencies.py
@@ -32,6 +32,7 @@ dependent_packages = {
     "xgboost": ("1.6", "tests", None),
     # TODO: remove condition when catboost supports python 3.11
     "catboost": ("1.0", "tests", "python_version < '3.11'"),
+    "fairlearn": ("0.8.0", "tests", None),
 }
 
 
