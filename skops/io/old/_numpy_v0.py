892b3fdcc083689d720abae04266ffaa2c7004f1
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-03-28 11:23:24 +0200

    FIX: Bug in random generator persistence
    One of its children was not persisted correctly (pure dict instead of
    DictNode), which made it impossible to check the untrusted types on it.
    This fix correctly stores the state as a node.
    
    Tests were extended to uncover this bug.
    
    The old persistence scheme was preserved for protocol vesion 0 -- even
    though it is buggy, it can work under some circumstances.

---
+++ skops/io/old/_numpy_v0.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import Any, Sequence
+
+import numpy as np
+
+from skops.io._audit import Node
+from skops.io._utils import LoadContext, gettype
+
+PROTOCOL = 0
+
+
+class RandomGeneratorNode(Node):
+    def __init__(
+        self,
+        state: dict[str, Any],
+        load_context: LoadContext,
+        trusted: bool | Sequence[str] = False,
+    ) -> None:
+        super().__init__(state, load_context, trusted)
+        self.children = {"bit_generator_state": state["content"]["bit_generator"]}
+        self.trusted = self._get_trusted(trusted, [np.random.Generator])
+
+    def _construct(self):
+        # first restore the state of the bit generator
+        bit_generator = gettype(
+            "numpy.random", self.children["bit_generator_state"]["bit_generator"]
+        )()
+        bit_generator.state = self.children["bit_generator_state"]
+
+        # next create the generator instance
+        return gettype(self.module_name, self.class_name)(bit_generator=bit_generator)
+
+
+NODE_TYPE_MAPPING = {
+    ("RandomGeneratorNode", PROTOCOL): RandomGeneratorNode,
+}
