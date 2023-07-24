f39816b311864259c8e442a1029987dad8f96680
Reid Johnson <reidj@zillowgroup.com>
2023-07-24 10:14:50 -0700

    Linting

--- skops/io/_quantile_forest.py
+++ skops/io/_quantile_forest.py
@@ -5,15 +5,14 @@ from typing import Any, Sequence
 from quantile_forest._quantile_forest_fast import QuantileForest
 
 from ._protocol import PROTOCOL
-
-from ._sklearn import reduce_get_state, ReduceNode
+from ._sklearn import ReduceNode, reduce_get_state
 from ._utils import LoadContext, SaveContext, get_module
 
 
 def quantile_forest_get_state(
-        obj: Any,
-        save_context: SaveContext,
-    ) -> dict[str, Any]:
+    obj: Any,
+    save_context: SaveContext,
+) -> dict[str, Any]:
     state = reduce_get_state(obj, save_context)
     state["__loader__"] = "QuantileForestNode"
     return state
@@ -39,9 +38,7 @@ class QuantileForestNode(ReduceNode):
 
 
 # tuples of type and function that gets the state of that type
-GET_STATE_DISPATCH_FUNCTIONS = [
-    (QuantileForest, quantile_forest_get_state)
-]
+GET_STATE_DISPATCH_FUNCTIONS = [(QuantileForest, quantile_forest_get_state)]
 
 # tuples of type and function that creates the instance of that type
 NODE_TYPE_MAPPING = {("QuantileForestNode", PROTOCOL): QuantileForestNode}
