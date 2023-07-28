c19c7ff1165e5d2c9a66ca3bb1d292df423d95c7
Reid Johnson <reidj@zillowgroup.com>
2023-07-28 08:18:31 -0700

    Update lazy loading

--- skops/io/_quantile_forest.py
+++ skops/io/_quantile_forest.py
@@ -6,44 +6,46 @@ from ._protocol import PROTOCOL
 from ._sklearn import ReduceNode, reduce_get_state
 from ._utils import LoadContext, SaveContext
 
-
-def _lazy_import():
-    from quantile_forest._quantile_forest_fast import QuantileForest
-
-    def quantile_forest_get_state(
-        obj: Any,
-        save_context: SaveContext,
-    ) -> dict[str, Any]:
-        state = reduce_get_state(obj, save_context)
-        state["__loader__"] = "QuantileForestNode"
-        return state
-
-    class QuantileForestNode(ReduceNode):
-        def __init__(
-            self,
-            state: dict[str, Any],
-            load_context: LoadContext,
-            trusted: bool | Sequence[str] = False,
-        ) -> None:
-            super().__init__(
-                state,
-                load_context,
-                constructor=QuantileForest,
-                trusted=trusted,
-            )
-            self.trusted = self._get_trusted(trusted, [])
-
-    return QuantileForest, QuantileForestNode, quantile_forest_get_state
-
-
 try:
-    QuantileForest, QuantileForestNode, quantile_forest_get_state = _lazy_import()
+    from quantile_forest._quantile_forest_fast import QuantileForest
+except ImportError:
+    QuantileForest = None
 
-    # tuples of type and function that gets the state of that type
+
+def quantile_forest_get_state(
+    obj: Any,
+    save_context: SaveContext,
+) -> dict[str, Any]:
+    state = reduce_get_state(obj, save_context)
+    state["__loader__"] = "QuantileForestNode"
+    return state
+
+
+class QuantileForestNode(ReduceNode):
+    def __init__(
+        self,
+        state: dict[str, Any],
+        load_context: LoadContext,
+        trusted: bool | Sequence[str] = False,
+    ) -> None:
+        if QuantileForest is None:
+            raise ImportError(
+                "`quantile_forest` is missing and needs to be installed in order to"
+                " load this model."
+            )
+
+        super().__init__(
+            state,
+            load_context,
+            constructor=QuantileForest,
+            trusted=trusted,
+        )
+        self.trusted = self._get_trusted(trusted, [])
+
+
+# tuples of type and function that gets the state of that type
+if QuantileForest is not None:
     GET_STATE_DISPATCH_FUNCTIONS = [(QuantileForest, quantile_forest_get_state)]
 
-    # tuples of type and function that creates the instance of that type
-    NODE_TYPE_MAPPING = {("QuantileForestNode", PROTOCOL): QuantileForestNode}
-except ModuleNotFoundError:
-    GET_STATE_DISPATCH_FUNCTIONS = []
-    NODE_TYPE_MAPPING = {}
+# tuples of type and function that creates the instance of that type
+NODE_TYPE_MAPPING = {("QuantileForestNode", PROTOCOL): QuantileForestNode}
