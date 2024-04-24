07e0d5ae4aa90f62efd41e1028ee5ef2ce21dabe
Thomas Lazarus <lazarustda@gmail.com>
2024-04-23 20:32:10 -0500

    Update scikeras version to 0.12.0 in _min_dependencies.py

--- skops/_min_dependencies.py
+++ skops/_min_dependencies.py
@@ -37,7 +37,7 @@ dependent_packages = {
     "catboost": ("1.0", "tests", None),
     "fairlearn": ("0.7.0", "docs, tests", None),
     "rich": ("12", "tests, rich", None),
-    "scikeras": ("0.13.0", "docs, tests", None),
+    "scikeras": ("0.12.0", "docs, tests", None),
     "tensorflow": ("2.16.0", "docs, tests", None),
 }
 
