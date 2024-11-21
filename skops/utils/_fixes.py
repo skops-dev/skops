926f9721c3860cc728aed00cd08f371933d455b2
Tamara <atanasoska.tamara@gmail.com>
2024-11-21 15:31:02 +0100

    Move imports for construct_instances

--- skops/utils/_fixes.py
+++ skops/utils/_fixes.py
@@ -1,9 +1,3 @@
-try:
-    from sklearn.utils._test_common.instance_generator import _construct_instances
-except ImportError:
-    from sklearn.utils.estimator_checks import _construct_instances
-
-
 def boxplot(ax, *, tick_labels, **kwargs):
     """A function to handle labels->tick_labels deprecation.
     labels is deprecated in 3.9 and removed in 3.11.
@@ -17,6 +11,11 @@ def boxplot(ax, *, tick_labels, **kwargs):
 def construct_instances(estimator):
     """Added for sklearn<1.6 support."""
     try:
+        from sklearn.utils._test_common.instance_generator import _construct_instances
+
         return next(_construct_instances(estimator))
-    except TypeError:
+
+    except ImportError:
+        from sklearn.utils.estimator_checks import _construct_instances
+
         return _construct_instances(estimator)
