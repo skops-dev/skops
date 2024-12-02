bcc78d4669ea69459377c21a8a1e8b8c7e79bcbd
adrinjalali <adrin.jalali@gmail.com>
2024-12-02 13:03:10 +0100

    ...

--- skops/utils/_fixes.py
+++ skops/utils/_fixes.py
@@ -3,6 +3,97 @@ from __future__ import annotations
 import sys
 from dataclasses import dataclass, field
 
+try:
+    from sklearn.utils.estimator_checks import (
+        _enforce_estimator_tags_X,
+        _enforce_estimator_tags_y,
+    )
+except ImportError:
+    import numpy as np
+    from sklearn.metrics.pairwise import linear_kernel, pairwise_distances
+
+    def _enforce_estimator_tags_y(estimator, y):
+        # Estimators with a `requires_positive_y` tag only accept strictly positive
+        # data
+        tags = get_tags(estimator)
+        if tags.target_tags.positive_only:
+            # Create strictly positive y. The minimal increment above 0 is 1, as
+            # y could be of integer dtype.
+            y += 1 + abs(y.min())
+        if (
+            tags.classifier_tags is not None
+            and not tags.classifier_tags.multi_class
+            and y.size > 0
+        ):
+            y = np.where(y == y.flat[0], y, y.flat[0] + 1)
+        # Estimators in mono_output_task_error raise ValueError if y is of 1-D
+        # Convert into a 2-D y for those estimators.
+        if tags.target_tags.multi_output and not tags.target_tags.single_output:
+            return np.reshape(y, (-1, 1))
+        return y
+
+    def _enforce_estimator_tags_X(estimator, X, X_test=None, kernel=linear_kernel):
+        def _is_pairwise_metric(estimator):
+            """Returns True if estimator accepts pairwise metric.
+
+            Parameters
+            ----------
+            estimator : object
+                Estimator object to test.
+
+            Returns
+            -------
+            out : bool
+                True if _pairwise is set to True and False otherwise.
+            """
+            metric = getattr(estimator, "metric", None)
+
+            return bool(metric == "precomputed")
+
+        # Estimators with `1darray` in `X_types` tag only accept
+        # X of shape (`n_samples`,)
+        if get_tags(estimator).input_tags.one_d_array:
+            X = X[:, 0]
+            if X_test is not None:
+                X_test = X_test[:, 0]  # pragma: no cover
+        # Estimators with a `requires_positive_X` tag only accept
+        # strictly positive data
+        if get_tags(estimator).input_tags.positive_only:
+            X = X - X.min()
+            if X_test is not None:
+                X_test = X_test - X_test.min()  # pragma: no cover
+        if get_tags(estimator).input_tags.categorical:
+            dtype = np.float64 if get_tags(estimator).input_tags.allow_nan else np.int32
+            X = np.round((X - X.min())).astype(dtype)
+            if X_test is not None:
+                X_test = np.round((X_test - X_test.min())).astype(
+                    dtype
+                )  # pragma: no cover
+
+        if estimator.__class__.__name__ == "SkewedChi2Sampler":
+            # SkewedChi2Sampler requires X > -skewdness in transform
+            X = X - X.min()
+            if X_test is not None:
+                X_test = X_test - X_test.min()  # pragma: no cover
+
+        X_res = X
+
+        # Pairwise estimators only accept
+        # X of shape (`n_samples`, `n_samples`)
+        if _is_pairwise_metric(estimator):
+            X_res = pairwise_distances(X, metric="euclidean")
+            if X_test is not None:
+                X_test = pairwise_distances(
+                    X_test, X, metric="euclidean"
+                )  # pragma: no cover
+        elif get_tags(estimator).input_tags.pairwise:
+            X_res = kernel(X, X)
+            if X_test is not None:
+                X_test = kernel(X_test, X)  # pragma: no cover
+        if X_test is not None:
+            return X_res, X_test
+        return X_res
+
 
 def boxplot(ax, *, tick_labels, **kwargs):
     """A function to handle labels->tick_labels deprecation.
