921dd934c01eea9ebf3a2a7f8552fd8af6b3ebde
Kyle Murphy <murphy.kyle.r@gmail.com>
2025-03-19 14:12:16 -0400

    Adding is_trusted function
    Adding functionality to determine if datatype is trusted in the skops framework.
    
    Intended to help users determine what they are able to save safely so that a model can be loaded without the need to okay/verify untrusted datatypes.

--- skops/io/__init__.py
+++ skops/io/__init__.py
@@ -1,4 +1,5 @@
-from ._persist import dump, dumps, get_untrusted_types, load, loads
+from ._persist import dump, dumps, get_untrusted_types, load, loads, is_trusted
 from ._visualize import visualize
 
-__all__ = ["dumps", "load", "loads", "dump", "get_untrusted_types", "visualize"]
+__all__ = ["dumps", "load", "loads", "dump", "get_untrusted_types", 
+           "visualize","is_trusted"]
